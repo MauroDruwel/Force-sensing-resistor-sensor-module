2250492b91fcd2799483298623ea29abb71ce82f
codermauro <mauro.druwel@gmail.com>
2021-12-26 20:36:02 +0100

    Add example

--- examples/WPSE477_example.ino
+++ examples/WPSE477_example.ino
@@ -9,14 +9,13 @@
   @@@@@@@@@@@@@@@@@@@@@@             @@@@@@@@@@@@@@@@@@@     @@@@    @@@@      @@@@    @@@@@      @@@@@@@@@@@@@@     @@@@@@@@@@@@@@      @@@@     @@@@
   @@@@@@@@@@@@@@@@@@@@@@
   
-  Whadda PHOTOSENSITIVE SENSOR MODULE
-  
+  Whadda FORCE SENSING RESISTOR MODULE
  */
 
 // These constants won't change. They're used to give names to the pins used:
-const int analogInPin = A0;  // Analog input pin that the light sensor is attached to
+const int analogInPin = A0;  // Analog input pin that the FSR is attached to
 
-int sensorValue = 0;        // value read from the light sensor
+int sensorValue = 0;        // value read from the FSR
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -26,11 +25,11 @@ void setup() {
 void loop() {
   // read the analog in value:
   sensorValue = analogRead(analogInPin);
-
+  
   // print the results to the Serial Monitor:
   Serial.print("sensor = ");
   Serial.println(sensorValue);
-
+  
   // wait 10 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   delay(10);
